a92f383b0476f192bddeced614b4a529d1c2ded6
darklinden <darklinden@gmail.com>
2024-10-09 18:35:19 +0800

    feat: add compose for debug; add test html

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM nginx:alpine
 
-COPY nginx/nginx.conf  /etc/nginx/nginx.conf
+COPY nginx/nginx.conf /etc/nginx/nginx.conf
+
+RUN mkdir -p /etc/nginx/media
+COPY nginx/media/favicon.ico /etc/nginx/media/favicon.ico
+
 RUN rm -rf /etc/nginx/conf.d/*
 COPY nginx/conf.d/* /etc/nginx/conf.d/
+
 COPY ssl /etc/nginx/ssl
+
+RUN mkdir -p /usr/share/html
+RUN rm -rf /usr/share/html/*
+COPY test/index.html /usr/share/html/index.html
 
--- acme-local.sh
+++ acme-local.sh
@@ -26,17 +26,17 @@ fi
 
 ACME_DIR="$PROJECT_DIR/acme"
 
-docker run -it \
+docker run -it --rm \
     -v "$ACME_DIR":/acme.sh \
     neilpang/acme.sh --set-default-ca --server letsencrypt
 
-docker run -it \
+docker run -it --rm \
     -v "$ACME_DIR":/acme.sh \
     -e CF_Token=$CF_Token \
     -e CF_Zone_ID=$CF_Zone_ID \
     neilpang/acme.sh --issue --dns dns_cf -d local.darklinden.site
 
-docker run -it \
+docker run -it --rm \
     -v "$ACME_DIR":/acme.sh \
     -e CF_Token=$CF_Token \
     -e CF_Zone_ID=$CF_Zone_ID \
